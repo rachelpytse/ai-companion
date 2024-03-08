8ef78e8fdc845955051db624fd396c019cfc58f3
rach <rachelpytse@gmail.com>
2024-03-08 09:15:43 +0100

    Added authentication to edit and delete

--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,5 +1,6 @@
 import prismadb from "@/lib/prismadb";
 import { CompanionForm } from "./components/companion-form";
+import { auth, redirectToSignIn } from "@clerk/nextjs";
 
 interface CompanionIdPageProps {
     params: {
@@ -10,11 +11,17 @@ interface CompanionIdPageProps {
 const CompanionIdPage = async ({
     params
 }: CompanionIdPageProps) => {
+    const {userId} = auth()
     //TODO: Check subscription
+
+    if(!userId) {
+        return redirectToSignIn()
+    }
 
     const compagion = await prismadb.companion.findUnique({
         where: {
             id: params.companionId,
+            userId
         }
     })
 
